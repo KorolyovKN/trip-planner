c9253d0972ecfda42401ff9cc0db5e46fc239123
Konstantin_Korolyov <konstantin.korolyov@windmill.ch>
2017-10-04 17:36:27 +0200

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+// DATABASE SETUP
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://node:node@ds145293.mlab.com:45293/my-test-database'); // connect to our database
+
+var Bear     = require('../models/bear');
+
+// Handle the connection event
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("DB connection alive");
+});
+
+router.get('/posts', function (req, res) {
+  Bear.find(function (err, bears) {
+    if(err)
+      res.send(err);
+    res.json(bears);
+  })
+})
+
+module.exports = router;
